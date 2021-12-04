2303a54511b0fdadae25022173a2fd4d4acd3a2a
conspir0 <michalczyk.mat3usz@gmail.com>
2021-12-04 20:08:24 +0100

    Create routing

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+const About = () => {
+  return ( 
+    <>
+      <div>About</div>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias officia voluptatem tenetur aspernatur. Est libero doloremque quo velit soluta praesentium accusamus quibusdam odio explicabo corporis, illo ipsa, incidunt commodi impedit?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias officia voluptatem tenetur aspernatur. Est libero doloremque quo velit soluta praesentium accusamus quibusdam odio explicabo corporis, illo ipsa, incidunt commodi impedit?</p>
+    </>
+   );
+}
+ 
+export default About;
