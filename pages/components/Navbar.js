4e1166a719f51f371331a69085d742c8b31c1592
conspir0 <michalczyk.mat3usz@gmail.com>
2021-12-04 20:12:47 +0100

    Create navbar

---
+++ pages/components/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return ( 
+    <nav>
+      <h1 className="logo">
+        Users list
+      </h1>
+      <a href="">Home</a>
+      <a href="">About</a>
+      <a href="">Users</a>
+    </nav>
+   );
+}
+ 
+export default Navbar;
