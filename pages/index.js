8fb68c00f55e54be463d7e1563639e42ac427645
conspir0 <michalczyk.mat3usz@gmail.com>
2021-12-04 20:19:36 +0100

    Create footer

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -9,9 +12,11 @@ export default function Home() {
         <meta name="description" content="Next JS - Simple page" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Navbar/>
       <main>
         <h1>Hello World!</h1>
       </main>
+      <Footer/>
     </div>
   )
 }
